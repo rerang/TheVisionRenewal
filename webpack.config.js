24485618c002280e7d4fdcf640a631138d462a0d
Rerang <64964484+rerang@users.noreply.github.com>
2022-11-26 10:47:38 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
   },
+  resolve: {
+        extensions: ['js','jsx']
+    },
   module: {
     rules: [
       { test: /\.js$/, use: 'babel-loader' },
