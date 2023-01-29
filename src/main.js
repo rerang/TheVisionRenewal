bf9c5906e18230d6dbb94d7c776b99898a8f0561
rerang <chemic00@naver.com>
2023-01-29 12:34:13 +0900

    conflict sloved

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import axios from 'axios';
 
 const app = createApp(App);
 
 app.use(router).mount('#app');
 
 app.config.globalProperties.MKURL = "http://3.39.153.21:8000/thevision/";
+app.config.globalProperties.$axios = axios;
