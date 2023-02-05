f2cde04f4a0952d68b1cbba41edc4bab29f086a7
rerang <chemic00@naver.com>
2023-02-05 13:53:07 +0900

    main slogan

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,69 +1,87 @@
 <template>
 <div class="mainContainer">
     <div class="pagesContainer">
-        <div class="sloganContainer">
-            <img class="mainBackgroundImg" src="../assets/img/flying_bird.png">
-            
-            <div class="sloganArea">
-                <div class="slogan_left" :style="{transform:sloganLeftStyle}">Be Competitive,</div>
-                <div class="slogan_right" :style="{transform:sloganRightStyle}">Move Forward</div>
+        <div class="bgCover"></div>  
+        <div class="pagesWrapper">
+            <div class="sloganContainer">          
+                <div class="sloganArea">
+                    <div class="slogan_left" :style="{transform:sloganLeftStyle, transition: '0.2s ease-out'}">Be Competitive,</div>
+                    <div class="slogan_right" :style="{transform:sloganRightStyle, transition: '0.2s ease-out'}">Move Forward</div>
+                </div>
             </div>
-        </div>
-        
-        <div class="introductionContainer">
-            <div class="space">
-                
+            <div class="introductionContainer">
+                asdfkl;jasd
             </div>
-            
         </div>
     </div>
-    
 </div>
 </template>
     
 <script setup> 
     import { ref, onMounted, watch } from 'vue';
+    import sloganBg from '../assets/img/logo_white.png'
 
-    const sloganMoveVW = 3;
-    let sloganLeftVal = ref(-55);
-    let sloganRightVal = ref(+55);
+    const sloganMoveVW = 6;
+    let sloganLeftVal = ref(-20);
+    let sloganRightVal = ref(+20);
     let sloganLeftStyle = ref('translateX(' + sloganLeftVal.value + 'vw)');
     let sloganRightStyle = ref('translateX(' + sloganRightVal.value + 'vw)');
     watch(sloganLeftVal, ()=>{
         sloganLeftStyle.value = 'translateX(' + sloganLeftVal.value + 'vw)';
         sloganRightStyle.value = 'translateX(' + sloganRightVal.value + 'vw)';
     })
+    let sloganBgOpacity = ref(0);
 
     
     let direction = ref(0);
-
     const getDirection = (event) => {
-        if(window.location.pathname == "/main")
-        window.scrollTo(0, 0);
-        direction.value = event.deltaY > 0 ? 1 : -1; //1: down, -1: up
-        moveSlogan();
+        if(window.location.pathname == "/Main"){
+            direction.value = event.deltaY > 0 ? 1 : -1; //1: down, -1: up
+        }
     }
+
+    let pageContent = ref("slogan");
+
     const moveSlogan = (event) => {
-        
+        let bgCover = document.querySelector(".bgCover");
+        let pagesWrapper = document.querySelector(".pagesWrapper");
+
         //start event 
         if(direction.value > 0){//scroll down
             if(sloganLeftVal.value == 100){
                 //change to intro
             }
-            sloganLeftVal.value += sloganMoveVW;
-            sloganRightVal.value -= sloganMoveVW;
+            if(sloganLeftVal.value < 100){
+                sloganLeftVal.value += sloganMoveVW;
+                sloganRightVal.value -= sloganMoveVW;
+            }else{
+                pagesWrapper.setAttribute("style", "transform: translateX(-100vw)");
+            }
+            if(sloganLeftVal.value > 25 && sloganBgOpacity.value <= 1 && sloganLeftVal.value < 100){
+                sloganBgOpacity.value += 0.1;
+            }else if(sloganLeftVal.value > 45){
+                sloganBgOpacity.value = 1;
+            }
         }else if(direction.value < 0){//scroll up
-            sloganLeftVal.value -= sloganMoveVW;
-            sloganRightVal.value += sloganMoveVW;
+            if(sloganLeftVal.value > -30){
+                sloganLeftVal.value -= sloganMoveVW;
+                sloganRightVal.value += sloganMoveVW;
+            }else{
+                sloganLeftVal.value = 5;
+                sloganRightVal.value = -5;
+            }
+            if(sloganLeftVal.value > 15 && sloganBgOpacity.value >= 0){
+                sloganBgOpacity.value -= 0.1;
+            }else if(sloganLeftVal.value > -10){
+                sloganBgOpacity.value = 0;
+            }
         }
-        // if(25~30){
-        //     새 만들기
-        // }
-
+        bgCover.setAttribute("style", "background:url(" + sloganBg + ") no-repeat center/45vw;" + "opacity:" + sloganBgOpacity.value + "");
     }
 
     onMounted(()=>{
-        window.addEventListener('mousewheel', getDirection);
+        window.addEventListener('mousewheel', (event)=>{getDirection(event); moveSlogan();});
+        document.querySelector(".bgCover").setAttribute("style", "background:url(" + sloganBg + ") no-repeat center/45vw;");
     })
 </script>
     
@@ -74,26 +92,41 @@
     }
     .pagesContainer{
         width: 100vw;
-        height: 100vh;
+        height: calc(100vh - 9rem);
+        box-sizing: border-box;
         overflow: hidden;
         padding: 0;
         display: flex;
-        flex-direction: row;
-        justify-content: center;                                         
+        flex-direction: row;                      
         align-items: center;
+    }
+    .bgCover{
+        width: 100vw;
+        height: calc(100vh - 9rem);
+        position: absolute;
+        opacity: 0;
+        transform: translateY(-3rem);
+    }
+    .pagesWrapper{
+        display: flex;
+        flex-direction: row;                      
+        align-items: center;
+        height: calc(100vh - 9rem);
     }
     .sloganContainer{
         width: 100vw;
+        height: calc(100vh - 9rem);
+        position: relative;
+        display: flex;
+        align-items: center;
 
-        .mainBackgroundImg{
-            display: none;
-        }
         .sloganArea{
             font-family: 'HeyAugust';
             color: #ffffff;
-            font-size: min(17rem, 250px);
-            width: 100%;
+            font-size: min(14rem, 250px);
+            width: 100vw;
             text-align: center;
+            position: relative;
             z-index: 2;
             overflow: hidden;
         }
@@ -102,10 +135,4 @@
         .slogan_right{
         }
     }
-    
-
-    .space{
-        min-height: 100vh;
-    }
-
 </style>
